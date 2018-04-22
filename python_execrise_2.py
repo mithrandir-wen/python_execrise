21de57d19b542719073be0144d1efea0faea6a3e
mithrandir-wen <wty901030@gmail.com>
2018-04-22 22:14:14 +0900

    功能增加与代码修改

--- python_execrise_2.py
+++ python_execrise_2.py
@@ -3,41 +3,61 @@
    Written by mithrandir-wen'''
 
 import itchat
+import pandas
+import re
+import jieba
 
 itchat.login()
-
 friends = itchat.get_friends(update=True)[0:] 
-#print (friends)
 
 def wechat_get_data(var):
-    city = []
+    data_list = []
     for i in friends[1:]:
-        city.append(i[var])
-    return city
+        data_list.append(i[var])
+    return data_list
 
 male = 0
 female = 0
 other = 0
 
-for i in friends[1:]:
-    sex = i["Sex"]
-    if sex == 1:
+sex_data_list = wechat_get_data("Sex")
+for i in sex_data_list:
+    if i == 1:
         male += 1
-    elif sex == 2:
+    elif i == 2:
         female += 1
     else:
         other += 1
-total = len(friends[1:])
+total = len(sex_data_list)
 
 print ("I have %d boys in my WeChat, about %.2f%%" %(male,float(male)/total*100))
 print ("I have %d girls in my WeChat, about %.2f%%" %(female,float(female)/total*100))
 print ("And %d unknown sex people exist in my Wechat, about %.2f%%" %(other,float(other/total*100)))
 
-'''for i in friends[1:]:
-    sex = i["Sex"]
-    name = i["NickName"]
-    if sex != 1 and i != 2:
-        print ("They are: {0}".format(name))'''
+city_data_list = wechat_get_data("City")
+nickname_data_list = wechat_get_data("NickName")
+province_data_list = wechat_get_data("Province")
+signature_data_list = wechat_get_data("Signature")
 
-print (wechat_get_data("City"))
+summary_data = {"Nickname":nickname_data_list,
+                "Sex":sex_data_list,
+                "City":city_data_list,
+                "Province":province_data_list,
+                "Signature":signature_data_list}
 
+PATH = "D:\wechat_data.csv"
+
+frame = pandas.DataFrame(summary_data)
+frame.to_csv(PATH,index=True)
+
+signature_new_list = []
+
+for i in signature_data_list:
+    signature = i.strip().replace("span","").replace("emoji","").replace("class","")
+    pattern = re.compile("1f\d+\w*|[<>/=]")
+    signature = pattern.sub("",signature)
+    signature_new_list.append(signature)
+signature_text = "".join(signature_new_list)
+
+signature_wordlist = jieba.cut(signature_text)
+signature_wordlist = " ".join(signature_wordlist)
